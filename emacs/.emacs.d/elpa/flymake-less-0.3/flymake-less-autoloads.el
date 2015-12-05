c3b75a509d0d00279c68432d98696b6595b96d7e
Maskim Burnin <Maksim.Burnin@gmail.com>
2015-12-05 17:40:51 +0500

    Moving emacs packages to Cask

--- emacs/.emacs.d/elpa/flymake-less-0.3/flymake-less-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; flymake-less-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (flymake-less-load) "flymake-less" "flymake-less.el"
-;;;;;;  (21343 14365 359138 755000))
-;;; Generated autoloads from flymake-less.el
-
-(autoload 'flymake-less-load "flymake-less" "\
-Flymake support for LESS files
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("flymake-less-pkg.el") (21343 14365 473059
-;;;;;;  355000))
-
-;;;***
-
-(provide 'flymake-less-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; flymake-less-autoloads.el ends here
