11c627942957f66d3f29ad51cecd96dc1aec566c
Maskim Burnin <Maksim.Burnin@gmail.com>
2016-12-11 18:00:50 +0500

    Add tpls to webmode alist

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -76,6 +76,7 @@
 (require 'less-css-mode)
   (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
